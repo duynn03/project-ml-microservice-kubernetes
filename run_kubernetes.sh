f31621ec8f4244cba2682c31d907ffdeeb8b5d1c
Duy Nguyen <duynn03@gmail.com>
2022-12-25 16:04:21 +0700

    completed Container Orchestration Project

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=duynn03/housing-price-predication
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run housing-price-predication --image=$dockerpath:latest --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward pod/housing-price-predication 8000:80
---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=duynn03/housing-price-predication
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag housing-price-predication $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
